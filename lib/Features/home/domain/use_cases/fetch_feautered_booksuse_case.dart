93306841e6bc1a005eaebcdf3ef3bb29153cf899
mohamed <mohamedhassouna88@gmail.com>
2023-10-17 02:25:13 -0700

    creatre use cases

--- lib/Features/home/domain/use_cases/fetch_feautered_booksuse_case.dart
+++ lib/Features/home/domain/use_cases/fetch_feautered_booksuse_case.dart
@@ -1,14 +1,16 @@
 import 'package:bookly_app_clean_arch/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app_clean_arch/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly_app_clean_arch/core/errors/failure.dart';
+import 'package:bookly_app_clean_arch/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeauteredBooksUseCase {
+class FetchFeauteredBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
   FetchFeauteredBooksUseCase(this.homeRepo);
 
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    return homeRepo.fetchFeaturedBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
